25c2ea9032f8c10f24e0d6760cda23623f50f317
BexTuychiev <bex@ibexprogramming.com>
2022-08-25 15:40:11 +0500

    enhance: Update generate_data function to save the data as a CSV

--- src/get_data.py
+++ src/get_data.py
@@ -12,4 +12,11 @@ def generate_data(n_samples=n_samples, n_features=n_features):
     X, y = make_classification(n_samples=n_samples, n_features=n_features,
                                n_informative=n_features, n_redundant=0, n_classes=2,
                                n_clusters_per_class=1)
-    return X, y
+    cols = [f'feat_{i}' for i in range(n_features)]
+    df = pd.DataFrame(X, columns=cols)
+    df['class'] = y
+    df.to_csv('data/data.csv', index=False)
+
+
+if __name__ == '__main__':
+    generate_data()
